abd1325f40dced69e06b0c40f1719ef791d0f503
Theedon <faniyantoyin@gmail.com>
2021-05-28 07:43:27 +0100

    Added an alert box that shows when user has successfully added information made the gender radio buttons work by using a toggle group with them

--- src/sample/RegisterUserController.java
+++ src/sample/RegisterUserController.java
@@ -1,19 +1,24 @@
 package sample;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 
+
+import java.net.URL;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
-public class RegisterUserController {
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class RegisterUserController implements Initializable {
+    Stage stage;
+
 
     @FXML
     private TextField idName;
@@ -27,17 +32,50 @@ public class RegisterUserController {
     @FXML
     private TextField idPhone;
 
-    Stage stage;
+    @FXML
+    private RadioButton Male;
 
-    public void onClickSubmit(javafx.event.ActionEvent event){
+    @FXML
+    private RadioButton Female;
+
+    ToggleGroup group;
+
+    public void initialize(URL location, ResourceBundle resources){
+        group= new ToggleGroup();
+        Male.setToggleGroup(group);
+        Female.setToggleGroup(group);
+
+
+    }
+
+
+
+
+
+    public void onClickSubmit(ActionEvent event){
         DatabaseConnection databaseConnection= new DatabaseConnection();
         Connection connection= databaseConnection.getConnection();
 
         String table_name= "phone_table";
         String name= idName.getText();
         String email= idEmail.getText();
-        String gender= idGender.getText();
+        String gender = null;
         String phone= idPhone.getText();
+
+        RadioButton selectedRadioButton= (RadioButton) group.getSelectedToggle();
+        String toggleGroupValue= selectedRadioButton.getText();
+
+        if(toggleGroupValue.equals("Male")){
+            gender= "M";
+        }
+        else if(toggleGroupValue.equals("Female")){
+            gender= "F";
+        }
+
+
+
+
+
 
 
         String query= "INSERT INTO "+table_name+"(name, email, gender, phone)" +
@@ -46,7 +84,10 @@ public class RegisterUserController {
         try{
             Statement statement= connection.createStatement();
             statement.executeUpdate(query);
-            System.out.println("done!");
+            //System.out.println("done!");
+            Alert alert= new Alert(Alert.AlertType.WARNING);
+            alert.show();
+            alert.setContentText("successful!");
             stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.close();
 
